8667e1d1127a7947bb12f4f4c04639411971de67
oonishireon <oonishireon110@gmail.com>
2020-12-25 03:05:00 +0900

    Initial Commit

---
+++ ShoppingApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingApplication
+//
+//  Created by 大西玲音 on 2020/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
