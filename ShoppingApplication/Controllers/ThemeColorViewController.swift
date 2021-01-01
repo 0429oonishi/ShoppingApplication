b58486c63821fc31b0fd2491646adb7c56061db8
oonishireon <oonishireon110@gmail.com>
2021-01-01 23:50:34 +0900

    Fix: All Conflict

--- ShoppingApplication/Controllers/ThemeColorViewController.swift
+++ ShoppingApplication/Controllers/ThemeColorViewController.swift
@@ -1,30 +1,27 @@
 
-
 import UIKit
-
-//デフォルトに戻すボタンを追加する
 
 class ThemeColorViewController: UIViewController {
     
-    @IBOutlet weak var themeColorNavigationBar: UINavigationBar!
+    @IBOutlet weak var themeColorNavigationBar: UINavigationBar! {
+        didSet { themeColorNavigationBar.tintColor = .black }
+    }
     @IBOutlet weak var themeColorDecisionButton: UIBarButtonItem!
-    @IBOutlet weak var themeColorCollectionView: UICollectionView!
+    @IBOutlet weak var themeColorCollectionView: UICollectionView! {
+        didSet {
+            themeColorCollectionView.delegate = self
+            themeColorCollectionView.dataSource = self
+        }
+    }
     private let themeColorCellId = "themeColorCellId"
     private let horizontalSpace = 40
     private var cellSize: Int { Int(self.view.bounds.width) / 3 - horizontalSpace }
-    var themeColorArray: [UIColor] = [#colorLiteral(red: 1, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.862745098, green: 0.07843137255, blue: 0.2352941176, alpha: 1), #colorLiteral(red: 1, green: 0.4117647059, blue: 0.7058823529, alpha: 1), #colorLiteral(red: 1, green: 0.07843137255, blue: 0.5764705882, alpha: 1), #colorLiteral(red: 1, green: 0.2705882353, blue: 0, alpha: 1), #colorLiteral(red: 1, green: 0.5490196078, blue: 0, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 0, alpha: 1), #colorLiteral(red: 0.9411764706, green: 0.9019607843, blue: 0.5490196078, alpha: 1), #colorLiteral(red: 0.5019607843, green: 0, blue: 0.5019607843, alpha: 1), #colorLiteral(red: 0.5411764706, green: 0.168627451, blue: 0.8862745098, alpha: 1), #colorLiteral(red: 0, green: 1, blue: 0.4980392157, alpha: 1), #colorLiteral(red: 0, green: 1, blue: 0, alpha: 1), #colorLiteral(red: 0, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 0.2509803922, green: 0.8784313725, blue: 0.8156862745, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 1, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0.5450980392, alpha: 1)]
-    var themeSelectedColor: UIColor?
+    private var selectedColor: UIColor?
+    private let themeColorArray: [UIColor] = [#colorLiteral(red: 1, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.862745098, green: 0.07843137255, blue: 0.2352941176, alpha: 1), #colorLiteral(red: 1, green: 0.07843137255, blue: 0.5764705882, alpha: 1), #colorLiteral(red: 1, green: 0.4117647059, blue: 0.7058823529, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 1, green: 0.2705882353, blue: 0, alpha: 1), #colorLiteral(red: 1, green: 0.5490196078, blue: 0, alpha: 1), #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 0, alpha: 1), #colorLiteral(red: 0.9411764706, green: 0.9019607843, blue: 0.5490196078, alpha: 1), #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1), #colorLiteral(red: 0.5019607843, green: 0, blue: 0.5019607843, alpha: 1), #colorLiteral(red: 0.5411764706, green: 0.168627451, blue: 0.8862745098, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0, green: 0.5628422499, blue: 0.3188166618, alpha: 1), #colorLiteral(red: 0, green: 1, blue: 0.4980392157, alpha: 1), #colorLiteral(red: 0, green: 1, blue: 0, alpha: 1), #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1), #colorLiteral(red: 0, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.2509803922, green: 0.8784313725, blue: 0.8156862745, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 1, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0.5450980392, alpha: 1)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        themeColorCollectionView.delegate = self
-        themeColorCollectionView.dataSource = self
-        
-        themeColorNavigationBar.tintColor = .black
-        
         themeColorCollectionViewLayout()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,7 +33,7 @@ class ThemeColorViewController: UIViewController {
     }
     
     @IBAction func tappedThemeColorDecisionButton(_ sender: Any) {
-        if let themeSelectedColor = themeSelectedColor {
+        if let themeSelectedColor = selectedColor {
             UserDefaults.standard.set(themeSelectedColor.rgbString, forKey: "themeColorKey")
         }
         dismiss(animated: true, completion: nil)
@@ -48,8 +45,6 @@ class ThemeColorViewController: UIViewController {
         layout.minimumLineSpacing = 30
         themeColorCollectionView.collectionViewLayout = layout
     }
-    
-    
 }
 
 extension ThemeColorViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -62,19 +57,18 @@ extension ThemeColorViewController: UICollectionViewDelegate, UICollectionViewDa
         let cell = themeColorCollectionView.dequeueReusableCell(withReuseIdentifier: themeColorCellId, for: indexPath)
         cell.layer.cornerRadius = CGFloat(cellSize/2)
         cell.backgroundColor = themeColorArray[indexPath.row]
-        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         for n in 0..<themeColorArray.count {
             switch indexPath.row {
-            case n: themeSelectedColor = themeColorArray[n]
+            case n: selectedColor = themeColorArray[n]
             default: break
             }
         }
-        self.view.backgroundColor = themeSelectedColor
-        themeColorNavigationBar.barTintColor = themeSelectedColor
+        self.view.backgroundColor = selectedColor
+        themeColorNavigationBar.barTintColor = selectedColor
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
