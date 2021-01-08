fa16d5e61003f89d1ef97691379b4a2692dc0094
oonishireon <oonishireon110@gmail.com>
2021-01-08 23:31:32 +0900

    使い方用の画像を追加

---
+++ ShoppingApplication/Controllers/HowToUseToBuyListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HowToUseToBuyListViewController.swift
+//  ShoppingApplication
+//
+//  Created by 大西玲音 on 2021/01/07.
+//
+
+import UIKit
+
+class HowToUseToBuyListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ShoppingApplication/Models/Place.swift
@@ -0,0 +1,29 @@
+//
+//  Place.swift
+//  ShoppingApplication
+//
+//  Created by 大西玲音 on 2021/01/08.
+//
+
+import UIKit
+
+class Place: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
