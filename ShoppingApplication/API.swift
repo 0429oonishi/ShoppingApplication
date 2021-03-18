776b0a0e0924e30c800df4aa392ef00a45e41ee9
0429oonishi <oonishireon110@gmail.com>
2021-03-19 06:27:37 +0900

    MapVCをリファクタリング

---
+++ ShoppingApplication/API.swift
@@ -0,0 +1,63 @@
+//
+//  API.swift
+//  ShoppingApplication
+//
+//  Created by 大西玲音 on 2021/03/19.
+//
+
+import Foundation
+import Alamofire
+
+final class API {
+    
+    static let shared = API()
+    private init() { }
+
+    func request(searchKeyword: String, lat: Double, lng: Double, completion: (([PlaceResults]) -> Void)? = nil) {
+        let baseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+        let apiKey = "AIzaSyA6zhP2dUBGYTQl1dJ8pjSJoyk67KnQil8"
+        let urlString = "\(baseURL)?location=\(lat),\(lng)&language=ja&radius=2000&keyword=\(searchKeyword)&key=\(apiKey)"
+        guard let encodeUrlString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        let request = AF.request(encodeUrlString)
+        request.responseJSON { response in
+            guard let data = response.data else { return }
+            let decoder = JSONDecoder()
+            let place = try! decoder.decode(Place.self, from: data)
+            completion?(place.results)
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ShoppingApplication/Controllers/MapViewController.swift
+++ ShoppingApplication/Controllers/MapViewController.swift
@@ -2,28 +2,26 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
-import Alamofire
+
+//themeColorを共通化
 
 final class MapViewController: UIViewController {
     
-    private enum Parameter: String {
-        case apiKey = "AIzaSyA6zhP2dUBGYTQl1dJ8pjSJoyk67KnQil8"
-        case baseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-    }
     private var themeColor: UIColor {
         guard let themeColorString = UserDefaults.standard.string(forKey: "themeColorKey") else {
             return .white
         }
         return UIColor(code: themeColorString)
     }
+    
     private var currentLocation = CLLocationManager()
     private var mapView = GMSMapView()
     private var placeResults = [PlaceResults]()
     private var userLocationLat: Double = 35.6812226
     private var userLocationLng: Double = 139.7670594
+    
     @IBOutlet weak var mapSearchBar: UISearchBar! {
         didSet {
-            mapSearchBar.delegate = self
             mapSearchBar.backgroundImage = UIImage()
             mapSearchBar.layer.cornerRadius = mapSearchBar.bounds.height / 2
             mapSearchBar.layer.borderWidth = 2
@@ -37,18 +35,23 @@ final class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapSearchBar.delegate = self
         setupMapView()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         self.view.backgroundColor = themeColor
         mapSearchBar.backgroundColor = themeColor
+        
     }
     
     private func setupMapView() {
-        let camera = GMSCameraPosition.camera(withLatitude: 35.6812226, longitude: 139.7670594, zoom: 17.0)
-        mapView = GMSMapView.map(withFrame: CGRect(x: 0.0, y: 0, width: self.view.frame.width, height: self.view.frame.height), camera: camera)
+        let camera = GMSCameraPosition.camera(withLatitude: userLocationLat, longitude: userLocationLng, zoom: 17.0)
+        mapView = GMSMapView.map(withFrame: CGRect(x: 0.0, y: 0.0, width: self.view.frame.width, height: self.view.frame.height), camera: camera)
         mapView.isMyLocationEnabled = true
         mapView.settings.myLocationButton = true
         currentLocation.delegate = self
@@ -63,6 +66,7 @@ final class MapViewController: UIViewController {
 }
 
 extension MapViewController: CLLocationManagerDelegate {
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation = locations.last
         userLocationLat = Double(userLocation!.coordinate.latitude)
@@ -71,23 +75,18 @@ extension MapViewController: CLLocationManagerDelegate {
         self.mapView.animate(to: camera)
         currentLocation.stopUpdatingLocation()
     }
+    
 }
 
 extension MapViewController: UISearchBarDelegate {
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         mapSearchBar.resignFirstResponder()
         mapView.clear()
-        let searchKeyword: String = searchBar.text!
-        let urlString = "\(Parameter.baseURL.rawValue)?location=\(userLocationLat),\(userLocationLng)&language=ja&radius=2000&keyword=\(searchKeyword)&key=\(Parameter.apiKey.rawValue)"
-        print(Parameter.apiKey)
-        guard let encodeUrlString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
-        let request = AF.request(encodeUrlString)
-        request.responseJSON { [self] (response) in
-            guard let data = response.data else { return }
-            let decoder = JSONDecoder()
-            let place = try! decoder.decode(Place.self, from: data)
-            placeResults = place.results
-            if placeResults.count != 0 {
+        
+        API.shared.request(searchKeyword: searchBar.text!, lat: userLocationLat, lng: userLocationLng) { placeResults in
+            self.placeResults = placeResults
+            if self.placeResults.count != 0 {
                 for n in 0...placeResults.count - 1 {
                     let latitude = placeResults[n].geometry.location.lat
                     let longitude = placeResults[n].geometry.location.lng
@@ -97,14 +96,15 @@ extension MapViewController: UISearchBarDelegate {
                     marker.title = placeResults[n].name
                     marker.snippet = placeResults[n].vicinity
                     marker.tracksViewChanges = true
-                    marker.map = mapView
+                    marker.map = self.mapView
                 }
-            }else {
+            } else {
                 let alert = UIAlertController(title: "周辺で探した結果", message: "検索結果は0です", preferredStyle: .alert)
                 let cancelAction = UIAlertAction(title: "閉じる", style: .cancel)
                 alert.addAction(cancelAction)
-                present(alert, animated: true)
+                self.present(alert, animated: true)
             }
         }
     }
+    
 }
